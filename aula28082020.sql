ea35abe47fd7fe5e572d5aa3ecb87ae330fdea3a
marcoaff10 <marcoafilho36@gmail.com>
2020-08-28 22:03:51 -0300

    Aula LPBD 28/08/2020

---
+++ aula28082020.sql
@@ -0,0 +1,41 @@
+select * from cliente;
+
+select * from vendedor;
+
+select * from produto;
+
+select * from pedido;
+
+select * from item_pedido;
+
+select c.cod_cli, c.nome_cli, p.num_ped from cliente c, pedido p where
+c.cod_cli = p.cod_cli
+order by c.nome_cli;
+
+select c.nome_cli, c.uf, p.num_ped, p.prazo_entr from cliente c, pedido p where
+c.cod_cli = p.cod_cli and
+p.prazo_entr > 15 and
+c.uf in ('sp', 'rj');
+
+select c.cod_cli, c.nome_cli, p.num_ped, p.prazo_entr from cliente c, pedido  p where
+c.cod_cli = p.cod_cli
+order by p.prazo_entr desc;
+
+select v.nome_vend, v.sal_fixo, p.num_ped, p.prazo_entr from vendedor v, pedido p where
+v.cod_vend = p.cod_vend and
+p.prazo_entr > 15 and
+v.sal_fixo >= 1000
+order by v.nome_vend;
+
+select c.nome_cli, c.uf, p.num_ped, p.prazo_entr, pr.desc_prod 
+from cliente c, pedido p, produto pr, item_pedido ip where 
+c.cod_cli = p.cod_cli and
+p.num_ped = ip.num_ped and
+pr.cod_prod = ip.cod_prod and
+pr.cod_prod = ip.cod_prod and
+c.uf = 'rj' and
+pr.desc_prod = 'chapa de Aco' and
+p.prazo_entr > 15 
+order by c.nome_cli;
+
+
